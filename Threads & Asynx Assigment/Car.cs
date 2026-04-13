5232e2e548ed17d6d8a2abe497cce1d951d44938
VISORKILLEN <viktor.gustavsson@live.se>
2026-04-13 13:26:50 +0200

    Fixed some langugage error and the speed boost

--- Threads & Asynx Assigment/Car.cs
+++ Threads & Asynx Assigment/Car.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Threads___Asynx_Assigment
+﻿namespace Threads___Asynx_Assigment
 {
     internal class Car
     {
@@ -19,6 +15,7 @@ namespace Threads___Asynx_Assigment
 
             Console.WriteLine($"{Name} starts!");
 
+            
             while (Distance < 5)
             {
                 Thread.Sleep(1000);
@@ -40,9 +37,33 @@ namespace Threads___Asynx_Assigment
 
         private void HandleEvent()
         {
+            int roll = random.Next(1, 51);
             
-
+            if (roll == 1)
+            {
+                Console.WriteLine($"{Name} is out of fuel, 15 sec stop to fuel the car.");
+                Thread.Sleep(15000);
+            }
+            else if (roll <= 3)
+            {
+                Console.WriteLine($"{Name} got a flat tire, 10 sec stop to change tire.");
+                Thread.Sleep(10000);
+            }
+            else if (roll <= 8)
+            {
+                Console.WriteLine($"{Name} got hit by a bird, 5 sec stop to clean the window");
+                Thread.Sleep(5000);
+            }
+            else if (roll <= 18)
+            {
+                Speed -= 1;
+                Console.WriteLine($"{Name} got enginge problems! Speed got reduced to {Speed} km/h");
+            }
+            else if (roll <= 26)
+            {
+                Speed += 2;
+                Console.WriteLine($"{Name} got a speed boost! Speed increased to {Speed}");
+            }
         }
-
     }
 }
